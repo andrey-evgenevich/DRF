e3460250611af0ea0f74585ece1cdde99a3b080a
Andrey <andreey.evgenevich.s@gmail.com>
2025-06-21 18:19:56 +0800

    ci.yml исправлен

--- users/serializers.py
+++ users/serializers.py
@@ -14,7 +14,7 @@ class CustomUserSerializer(ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = "__all__"
+        fields = ["email", 'phone', 'city']
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
